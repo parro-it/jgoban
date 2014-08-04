77359dec093cd17a88767b22154790bd7b65def9
Andrea Parodi <andrea.parodi@ebansoftware.net>
2014-08-04 13:41:18 +0200

    first release

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,19 +8,75 @@
 
 'use strict';
 
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
 
-gulp.task('test', function () {
-  return gulp.src('./test/*.js')
-    .pipe(mocha({
-      ui: 'bdd',
-      reporter: 'spec'
-    }));
+var $ = require('gulp-load-plugins')({
+    lazy: true
+});
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+
+gulp.task('build', ['copy-index', 'build-js', 'build-styles']);
+
+function guard(plugin) {
+    plugin.on('error', gutil.log);
+    return plugin;
+}
+
+gulp.task('build-js', function() {
+
+    return gulp.src('./lib/jgoban.js')
+        .pipe($.sourcemaps.init())
+        .pipe($.pureCjs({
+            output: 'jgoban.js',
+            exports: 'jgoban'
+        }).on('error', gutil.log))
+        .pipe($.sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('watch', function () {
-  gulp.watch(['./lib/**/*.js', './test/**/*.js'], ['test']);
+gulp.task('build-img', function() {
+    var spritesmith = require('gulp.spritesmith');
+    var spriteData = gulp.src('assets/img/normal/*.png').pipe(spritesmith({
+        imgName: 'jgoban.png',
+        cssName: 'img.less'
+    }));
+    spriteData.img.pipe(gulp.dest('dist'));
+    spriteData.css.pipe(gulp.dest('assets/styles'));
+});
+
+gulp.task('build-styles', function() {
+    return gulp.src('./assets/jgoban.less')
+        .pipe($.sourcemaps.init())
+        .pipe(guard($.less()))
+        .pipe($.sourcemaps.write('.'))
+        .pipe(gulp.dest('./dist'));
+
+
+});
+
+
+gulp.task('copy-index', function() {
+    return gulp.src('./assets/index.html', {
+            base: './assets'
+        })
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('test', function() {
+    return gulp.src('./test/*.js')
+        .pipe($.mocha({
+            ui: 'bdd',
+            reporter: 'spec'
+        }));
+});
+
+gulp.task('watch-test', function() {
+    return gulp.watch(['./lib/**/*.js', './test/**/*.js'], ['test']);
+});
+
+gulp.task('watch-build', function() {
+    return gulp.watch(['./lib/**/*.js', './test/**/*.js', './assets/**/*.*'], ['build']);
 });
 
 gulp.task('default', ['test', 'watch']);
+gulp.task('dev', ['watch-build'], $.serve(process.cwd()));
